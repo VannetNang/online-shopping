a8190e217a1d413d79f0eb863a9a17d85d03d600
VannetNang <NangVannet59@gmail.com>
2025-05-30 21:08:26 +0700

    setting up database and error middleware

--- Back-end/server.js
+++ Back-end/server.js
@@ -1,15 +1,16 @@
-import express, { urlencoded } from "express";
+import express from "express";
 import cors from "cors";
 import { PORT } from "./config/env.js";
 import { conectToDatabase } from "./database/mongodb.js";
 import userRouter from "./route/user.route.js";
+import errorMiddleware from "./middleware/error.middleware.js";
 
 const app = express();
 
 // Middle-ware
 app.use(cors());
 app.use(express.json());
-app.use(urlencoded({ extended: false }));
+app.use(express.urlencoded({ extended: false }));
 
 // API
 app.use("/api/v1/auth", userRouter);
@@ -17,6 +18,9 @@ app.use("/api/v1/auth", userRouter);
 app.get("/", (req, res) => {
   res.json({ message: "Welcome to Backend API!" });
 });
+
+// Error handler middleware (should be the last middleware)
+app.use(errorMiddleware);
 
 // Connection
 app.listen(PORT, () => {
